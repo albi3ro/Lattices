51872b741d5e93c9b3fead06ff08a32039cca274
albiero <chrissie.c.l@gmail.com>
2015-12-06 12:19:08 +0900

    adding other self package HAtom

--- Lattices.jl
+++ Lattices.jl
@@ -4,13 +4,13 @@ export Lattice, MakeLattice, PlotNeighbors
 
     type Lattice
         name::ASCIIString
-        l::Int16
-        dim::Int8
+        l::UInt8
+        dim::UInt8
         a::Array
         unit::Array
-        N::Int16
+        N::UInt16
         X::Array
-        nnei::Int8
+        nnei::UInt8
         neigh::Array
     end
     
@@ -19,7 +19,7 @@ export Lattice, MakeLattice, PlotNeighbors
     end
     
     
-    function MakeLattice(name,l)
+    function MakeLattice(name::ASCIIString,l::UInt8)
         if(name=="Square")
             return MakeSquare(l)
         elseif(name=="Chain")
@@ -38,11 +38,11 @@ export Lattice, MakeLattice, PlotNeighbors
     
     end
     
-    function MakeSquare(l)
-        N=l^2;
-        d=2;
+    function MakeSquare(l::UInt8)
+        N::UInt16=l^2;
+        d::UInt8=2;
         X=Array{Int16}(N,2);
-        nnei=4;
+        nnei::UInt8=4;
         neigh=Array{Int16}(N,4);
     
         X[:,1]=armod(collect(1:N),l);
@@ -59,11 +59,11 @@ export Lattice, MakeLattice, PlotNeighbors
         return Lattice("Square",l,d,a,unit,N,X,nnei,neigh)
     end 
     
-    function MakeChain(l)
-        N=l
-        d=1
-        nnei=2
-        neigh=Array{Int16}(N,2)
+    function MakeChain(l::UInt8)
+        N::UInt16=l
+        d::Int8=1
+        nnei::UInt8=2
+        neigh=Array{Int8}(N,2)
         a=[1];
         unit=[0];
                 
@@ -75,38 +75,73 @@ export Lattice, MakeLattice, PlotNeighbors
         return Lattice("Chain",l,d,a,unit,N,X,nnei,neigh)
     end
     
-    function MakeHoneycomb(l)
-        d=2;
-        nnei=3;
-        N=2*l^2;
+    function MakeHoneycomb(l::UInt8)
+        d::UInt8=2;
+        nnei::UInt8=3;
+        N::UInt16=2*l^2;
         a=[[2*cos(pi/6) 0]
             [cos(pi/6) 1+sin(pi/6)]];
         unit=[[0 0]
                 [cos(π/6) sin(π/6)]];
-
-        return GeneralMultiUnit2D("Honeycomb",l,d,a,unit,nnei)
+        
+        X=MakeX(a,unit,l,d);
+        
+        neigh=Array{Int16}(N,3);
+        for i in 1:l
+            neigh[2*i-1,1]=2*i;
+            neigh[2*i,1]=2*i-1;
+            
+            neigh[2*i+1,2]=2*i;
+            neigh[2*i,2]=2*i+1;
+        end
+        
+        for j in 1:l
+            for i in 1:l
+                neigh[2*i-1+2*l*(j-1),3]=armod(2*i+2*l*(j-2)+N,N);
+                neigh[2*i+2*l*(j-1),3]=armod(2*i+2*l*(j)+N-1,N);
+            end
+        end
+        
+        neigh[1,2]=2*l;
+        neigh[2*l,2]=1;
+        
+        for j in 2:l
+            neigh[(2*l*(j-1)+1):(2*l*j),1:2]=neigh[1:(2*l),1:2]+2*l*(j-1);
+        end
+        
+        return Lattice("Honeycomb",l,d,a,unit,N,X,nnei,neigh)
     end
     
-    function GeneralMultiUnit2D(name,l,d,a,unit,nnei)
+    function MakeX(a::Array,unit::Array,l::UInt8,d::UInt8)
         ncell=size(a)[1];
-        N=l*l*ncell;
+        N=ncell*l^d;
         
+        println(ncell,' ', typeof(ncell),' ',l, ' ',typeof(l))
         a1=repeat(a[1,:],outer=[ncell,1]);
         a2=repeat(a[2,:],outer=[ncell*l,1]);
+        if d==3
+            a3=repeat(a[3,:],outer=[ncell*l^2,1]);
+        end
         
-        X=Array{Float64}(N,2);
+        
+        X=Array{Float64}(N,d);
         # Here we are actually calculating the positions for every site
         for i in 1:l    #for the first row
             X[ncell*i-ncell+1:ncell*i,:]=unit+(i-1)*a1;
         end
         
         for j in 2:l    #copying the first row into the first layer
-            X[(ncell*l*(j-1)+1):(ncell*l*(j-1)+ncell*l),:]=X[1:ncell*l,:]+(j-1)*a2;
+            X[(ncell*l*(j-1)+1):(ncell*l*j),:]=X[1:ncell*l,:]+(j-1)*a2;
         end
         
-        neigh=Array{Int16}(N,nnei)
+        if d==3
+            for k in 2:l    #copying the first row into the first layer
+                X[(ncell*l^2*(k-1)+1):(ncell*l^2*k),:]=X[1:ncell*l^2,:]+(k-1)*a3;
+            end
+        end
         
-        return Lattice(name,l,d,a,unit,N,X,nnei,neigh)
+        
+        return X
     end
     
     
